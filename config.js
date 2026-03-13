07adddf1fd57aa7dc32c3a6dd6081a1631cffe89
odzre <iketutandikaputra07@gmail.com>
2026-03-13 11:40:52 +0000

    Add config.js via Rizky CodeX

---
+++ config.js
@@ -0,0 +1,14 @@
+export default {
+    botName: 'Choso Kamo — ODZRESHOP',
+    ownerNumber: ['6283162065090'],
+    usePairingCode: true,
+    thumbnail: 'assets/adreply.jpg',
+    title: 'Choso Kamo — ODZRESHOP',
+    body: 'WhatsApp Bot TopUp',
+    sourceUrl: 'https://github.com',
+    digiUser: '', 
+    digiKey: '-9be5-5c8f-8454-a78a83aba33',
+    orderKuotaUserName: 'reydhojunico',
+    orderKuotaToken: '2447632:QfdaPV4JBzXgUYRNZkxO6nsD3FAMS2pr',
+    qris: '00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214342075858612290303UMI51440014ID.CO.QRIS.WWW0215ID20254046695610303UMI5204541153033605802ID5921NEXUS STORE OK24476326008KUANSING61052951162070703A01630485C2', 
+}
